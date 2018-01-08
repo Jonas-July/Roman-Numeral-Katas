8ccdfb43a0dcc78e713744e780e80800ad5bfc1f
Jonas-July <jonas.july@yahoo.co.uk>
2018-01-09 00:19:17 +0100

    Added test for 10

--- RNK_try2/testRNK.cpp
+++ RNK_try2/testRNK.cpp
@@ -3,16 +3,17 @@
 
 int testRNK()
 {
-    if (convertArabicToRoman(0) != ""    ) return -1;
-    if (convertArabicToRoman(1) != "I"   ) return  1;
-    if (convertArabicToRoman(2) != "II"  ) return  2;
-    if (convertArabicToRoman(3) != "III" ) return  3;
-    if (convertArabicToRoman(4) != "IV"  ) return  4;
-    if (convertArabicToRoman(5) != "V"   ) return  5;
-    if (convertArabicToRoman(6) != "VI"  ) return  6;
-    if (convertArabicToRoman(7) != "VII" ) return  7;
-    if (convertArabicToRoman(8) != "VIII") return  8;
-    if (convertArabicToRoman(9) != "IX"  ) return  9;
+    if (convertArabicToRoman( 0) != ""    ) return -1;
+    if (convertArabicToRoman( 1) != "I"   ) return  1;
+    if (convertArabicToRoman( 2) != "II"  ) return  2;
+    if (convertArabicToRoman( 3) != "III" ) return  3;
+    if (convertArabicToRoman( 4) != "IV"  ) return  4;
+    if (convertArabicToRoman( 5) != "V"   ) return  5;
+    if (convertArabicToRoman( 6) != "VI"  ) return  6;
+    if (convertArabicToRoman( 7) != "VII" ) return  7;
+    if (convertArabicToRoman( 8) != "VIII") return  8;
+    if (convertArabicToRoman( 9) != "IX"  ) return  9;
+    if (convertArabicToRoman(10) != "X"   ) return 10;
 
     return 0;
 }

440b9f6fc05b5082755f03197ece8f737758f9ad
Jonas-July <jonas.july@yahoo.co.uk>
2018-01-09 00:03:55 +0100

    Converter converts 3 correctly

--- RNK_try2/testRNK.cpp
+++ RNK_try2/testRNK.cpp
@@ -3,9 +3,10 @@
 
 int testRNK()
 {
-    if (convertArabicToRoman(0) != ""  ) return -1;
-    if (convertArabicToRoman(1) != "I" ) return  1;
-    if (convertArabicToRoman(2) != "II") return  2;
+    if (convertArabicToRoman(0) != ""   ) return -1;
+    if (convertArabicToRoman(1) != "I"  ) return  1;
+    if (convertArabicToRoman(2) != "II" ) return  2;
+    if (convertArabicToRoman(3) != "III") return  3;
 
     return 0;
 }

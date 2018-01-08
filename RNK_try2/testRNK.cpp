f749efbd5ad2a9ed5646d8eef71d463965530e39
Jonas-July <jonas.july@yahoo.co.uk>
2018-01-09 00:33:20 +0100

    Added tests up to 20

--- RNK_try2/testRNK.cpp
+++ RNK_try2/testRNK.cpp
@@ -3,22 +3,27 @@
 
 int testRNK()
 {
-    if (convertArabicToRoman( 0) != ""    ) return -1;
-    if (convertArabicToRoman( 1) != "I"   ) return  1;
-    if (convertArabicToRoman( 2) != "II"  ) return  2;
-    if (convertArabicToRoman( 3) != "III" ) return  3;
-    if (convertArabicToRoman( 4) != "IV"  ) return  4;
-    if (convertArabicToRoman( 5) != "V"   ) return  5;
-    if (convertArabicToRoman( 6) != "VI"  ) return  6;
-    if (convertArabicToRoman( 7) != "VII" ) return  7;
-    if (convertArabicToRoman( 8) != "VIII") return  8;
-    if (convertArabicToRoman( 9) != "IX"  ) return  9;
-    if (convertArabicToRoman(10) != "X"   ) return 10;
-    if (convertArabicToRoman(11) != "XI"  ) return 11;
-    if (convertArabicToRoman(12) != "XII" ) return 12;
-    if (convertArabicToRoman(13) != "XIII") return 13;
-    if (convertArabicToRoman(14) != "XIV" ) return 14;
-    if (convertArabicToRoman(15) != "XV"  ) return 15;
+    if (convertArabicToRoman( 0) != ""     ) return -1;
+    if (convertArabicToRoman( 1) != "I"    ) return  1;
+    if (convertArabicToRoman( 2) != "II"   ) return  2;
+    if (convertArabicToRoman( 3) != "III"  ) return  3;
+    if (convertArabicToRoman( 4) != "IV"   ) return  4;
+    if (convertArabicToRoman( 5) != "V"    ) return  5;
+    if (convertArabicToRoman( 6) != "VI"   ) return  6;
+    if (convertArabicToRoman( 7) != "VII"  ) return  7;
+    if (convertArabicToRoman( 8) != "VIII" ) return  8;
+    if (convertArabicToRoman( 9) != "IX"   ) return  9;
+    if (convertArabicToRoman(10) != "X"    ) return 10;
+    if (convertArabicToRoman(11) != "XI"   ) return 11;
+    if (convertArabicToRoman(12) != "XII"  ) return 12;
+    if (convertArabicToRoman(13) != "XIII" ) return 13;
+    if (convertArabicToRoman(14) != "XIV"  ) return 14;
+    if (convertArabicToRoman(15) != "XV"   ) return 15;
+    if (convertArabicToRoman(16) != "XVI"  ) return 16;
+    if (convertArabicToRoman(17) != "XVII" ) return 17;
+    if (convertArabicToRoman(18) != "XVIII") return 18;
+    if (convertArabicToRoman(19) != "XIX"  ) return 19;
+    if (convertArabicToRoman(20) != "XX"   ) return 20;
 
     return 0;
 }

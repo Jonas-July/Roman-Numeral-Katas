397b2f871be7ce7ebb87489ca22c1ad4eb81336a
Jonas-July <jonas.july@yahoo.co.uk>
2018-01-09 02:13:20 +0100

    Put all steps together
    After generalization, everything is put together into one big, compact chunk of code

--- RNK_try2/Converter.cpp
+++ RNK_try2/Converter.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 #include "Converter.hpp"
 
@@ -9,60 +10,17 @@ std::string convertArabicToRoman(int arabic)
 
     std::string roman = "";
 
-    for (; arabic >= romanNumeralValues[0] - romanNumeralValues[1]; arabic -= romanNumeralValues[0])
+    for (uint16_t i = 0; i < romanNumerals.size(); i++)
     {
-        for (; arabic < romanNumeralValues[0]; arabic += romanNumeralValues[1])
+        for (; arabic >= romanNumeralValues[i] - romanNumeralValues[i + i % 2 + 1]; arabic -= romanNumeralValues[i])
         {
-            roman += romanNumerals[1];
+            for (; arabic < romanNumeralValues[i]; arabic += romanNumeralValues[i + i % 2 + 1])
+            {
+                roman += romanNumerals[i + i % 2 + 1];
+            }
+
+            roman += romanNumerals[i];
         }
-
-        roman += romanNumerals[0];
-    }
-
-
-    for (; arabic >= romanNumeralValues[1] - romanNumeralValues[3]; arabic -= romanNumeralValues[1])
-    {
-        for (; arabic < romanNumeralValues[1]; arabic += romanNumeralValues[3])
-        {
-            roman += romanNumerals[3];
-        }
-
-        roman += romanNumerals[1];
-    }
-
-    for (; arabic >= romanNumeralValues[2] - romanNumeralValues[3]; arabic -= romanNumeralValues[2])
-    {
-        for (; arabic < romanNumeralValues[2]; arabic += romanNumeralValues[3])
-        {
-            roman += romanNumerals[3];
-        }
-
-        roman += romanNumerals[2];
-    }
-
-    for (; arabic >= romanNumeralValues[3] - romanNumeralValues[5]; arabic -= romanNumeralValues[3])
-    {
-        for (; arabic < romanNumeralValues[3]; arabic += romanNumeralValues[5])
-        {
-            roman += romanNumerals[5];
-        }
-
-        roman += romanNumerals[3];
-    }
-
-    for (; arabic >= romanNumeralValues[4] - romanNumeralValues[5]; arabic -= romanNumeralValues[4])
-    {
-        for (; arabic < romanNumeralValues[4]; arabic += romanNumeralValues[5])
-        {
-            roman += romanNumerals[5];
-        }
-
-        roman += romanNumerals[4];
-    }
-
-    for (; arabic >= 1; arabic -= 1)
-    {
-        roman += "I";
     }
 
     return roman;

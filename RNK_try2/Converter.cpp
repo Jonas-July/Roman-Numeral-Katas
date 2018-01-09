d5dd2e1ecacf9ab09daafa35ac727b321a357c4e
Jonas-July <jonas.july@yahoo.co.uk>
2018-01-09 02:44:31 +0100

    Now works with 900

--- RNK_try2/Converter.cpp
+++ RNK_try2/Converter.cpp
@@ -5,18 +5,18 @@
 
 std::string convertArabicToRoman(int arabic)
 {
-    std::vector<int>         romanNumeralValues = {500, 100,  50,  10,  5 ,  1 };
-    std::vector<std::string> romanNumerals      = {"D", "C", "L", "X", "V", "I"};
+    std::vector<int>         romanNumeralValues = {1000, 500, 100,  50,  10,  5 ,  1 };
+    std::vector<std::string> romanNumerals      = { "M", "D", "C", "L", "X", "V", "I"};
 
     std::string roman = "";
 
     for (uint16_t i = 0; i < romanNumerals.size(); i++)
     {
-        for (; arabic >= romanNumeralValues[i] - romanNumeralValues[i + i % 2 + 1]; arabic -= romanNumeralValues[i])
+        for (; arabic >= romanNumeralValues[i] - romanNumeralValues[i + 1 + (i + romanNumerals.size()) % 2]; arabic -= romanNumeralValues[i])
         {
-            for (; arabic < romanNumeralValues[i]; arabic += romanNumeralValues[i + i % 2 + 1])
+            for (; arabic < romanNumeralValues[i]; arabic += romanNumeralValues[i + 1 + (i + romanNumerals.size()) % 2])
             {
-                roman += romanNumerals[i + i % 2 + 1];
+                roman += romanNumerals[i + 1 + (i + romanNumerals.size()) % 2];
             }
 
             roman += romanNumerals[i];
